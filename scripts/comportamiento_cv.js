64977322b163996f8816f23efa73cea09b5de383
rlopezca <ramonlopezc@gmail.com>
2023-02-28 22:16:35 -0300

    Capa comportamiento y cambios estructura HTML

--- scripts/comportamiento_cv.js
+++ scripts/comportamiento_cv.js
@@ -1,1 +1,18 @@
-document.getElementById ("foto_perfil").togle
+const btnPerfil = document.getElementById ("btnPerfil");
+const fotoPerfil = document.getElementById ("fotoPerfil");
+
+btnPerfil.addEventListener ("click", cambiaFoto); 
+// Al mantener botón presionado, cambiar a foto alternativa; ATENCIÓN: si agregamos "()" tras la función, se disparará al cargar la página y:...
+
+// btnPerfil.onclick= cambiaFoto ()
+btnPerfil.addEventListener ("click", cambiaTxtBtn); // ...cambiar leyenda botón
+
+// document.getElementById ("btnPerfil").onclick= cambiaFoto ();
+function cambiaFoto () {
+    fotoPerfil.src= "imagenes/cat-2083492_rsz128.jpg";
+}
+
+function cambiaTxtBtn () {
+    btnPerfil.innerHTML = "Conocé a mi humano"
+}
+
