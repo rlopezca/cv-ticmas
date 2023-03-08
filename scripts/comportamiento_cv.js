7d887daf3b7e2187b5c8c34f15509b08bf1a06d4
rlopezca <ramonlopezc@gmail.com>
2023-03-08 10:36:26 -0300

    js botón alternador

--- scripts/comportamiento_cv.js
+++ scripts/comportamiento_cv.js
@@ -1,18 +1,32 @@
-const btnPerfil = document.getElementById ("btnPerfil");
+const btnPerfil = document.getElementById ("btnPerfil"); 
 const fotoPerfil = document.getElementById ("fotoPerfil");
+let alternativa = true // Variable que servirá de condición para alternar efectos de otras funciones
 
-btnPerfil.addEventListener ("click", cambiaFoto); 
-// Al mantener botón presionado, cambiar a foto alternativa; ATENCIÓN: si agregamos "()" tras la función, se disparará al cargar la página y:...
-
-// btnPerfil.onclick= cambiaFoto ()
-btnPerfil.addEventListener ("click", cambiaTxtBtn); // ...cambiar leyenda botón
-
-// document.getElementById ("btnPerfil").onclick= cambiaFoto ();
-function cambiaFoto () {
-    fotoPerfil.src= "imagenes/cat-2083492_rsz128.jpg";
+function alternador () { // alterna el valor de verdad de la variable; i.e. arroja su complemento lógico
+    alternativa = !alternativa;
 }
 
-function cambiaTxtBtn () {
-    btnPerfil.innerHTML = "Conocé a mi humano"
+function cambiaImg () { //según el estado de la variable condicionante (`alternativa`), mostrará una u otra imagen
+    if (alternativa) {
+        fotoPerfil.src= "https://randomuser.me/api/portraits/men/72.jpg";
+    }
+    else {
+        fotoPerfil.src= "imagenes/cat-2083492_rsz128.jpg";
+    }
 }
 
+function cambiaTxtBtn () { //idem cambiaImg pero para leyenda de botón
+    if (alternativa) {
+        btnPerfil.innerHTML = "Conocé a mi mascota";
+    }
+    else {
+        btnPerfil.innerHTML = "Conocé a mi humano";
+    }  
+}
+
+btnPerfil.addEventListener ("click", alternador); //cliquear botón producirá cambio de estado en la variable (`alternativa`), se reasigna a su complemnto lógico (si era `true` pasa a `false` y viceversa)
+
+btnPerfil.addEventListener ("click", cambiaImg); // (ATENCIÓN: si agregamos "()" tras la función, se disparará al cargar la página.) Al presionar botón, cambiar a foto alternativa y:... 
+
+btnPerfil.addEventListener ("click", cambiaTxtBtn); 
+// ...cambiar leyenda botón
